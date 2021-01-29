5cde5fd08892d7dd11677fe405d08ef7e0cc86c6
Антон Берестов <8berestov8@gmail.com>
2021-01-29 20:39:58 +0400

    update put

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,17 @@
 import axios from 'axios'
 
-const baseUrl = 'https://course-3-a6229-default-rtdb.firebaseio.com/tasks.json'
+const baseUrl = 'https://course-3-a6229-default-rtdb.firebaseio.com/'
 
 export default {
   tasks: {
     get() {
-      return axios.get(baseUrl)
+      return axios.get(baseUrl + `tasks.json`)
     },
     post(data) {
-      return axios.post(baseUrl, data)
+      return axios.post(baseUrl + `tasks.json`, data)
     },
     put(data) {
-      // Нет понимания как отправлять запросы по обновлению
-      return axios.put(baseUrl + '/' + data.id, {data})
+      return axios.put(baseUrl + `tasks/${data.id}.json`, data)
     }
   }
 }
